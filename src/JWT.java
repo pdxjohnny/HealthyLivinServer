6300b469656ab97611a20e0d0d2095fdaf0cdd41
John Andersen <johnandersenpdx@gmail.com>
2016-05-22 11:44:43 +0000

    Working on JWT and JSON classes
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ src/JWT.java
@@ -0,0 +1,66 @@
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Map;
+
+/**
+ * Created by John Andersen on 5/22/16.
+ */
+public class JWT {
+    public static final String JWT_FAILURE = "false";
+    public static final String JWT_CHARSET = "UTF-8";
+
+    // Env vars
+    public static final String JWT_SECRET = "JWT_SECRET";
+
+    public String CHARSET;
+
+    private String tokenSecret;
+
+    JWT(String mTokenSalt, String charset) {
+        tokenSecret = mTokenSalt;
+        CHARSET = charset;
+    }
+
+    public static JWT fromEnv() {
+        return new JWT(System.getenv(JWT_SECRET), JWT_CHARSET);
+    }
+
+    public String toString(User user) {
+        String tokenHeaders = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
+        String tokenPayload = String.format("{\"uid\": \"%d\",\"username\": \"%s\"}", user.getId(), user.getUsername());
+
+        // Base64 encode them all
+        Base64.Encoder encoder = Base64.getUrlEncoder();
+        try {
+            tokenHeaders = encoder.encodeToString(tokenHeaders.getBytes(CHARSET));
+            tokenPayload = encoder.encodeToString(tokenPayload.getBytes(CHARSET));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return JWT_FAILURE;
+        }
+
+        // Add the secret
+        String tokenSignature = String.format("%s.%s", tokenHeaders, tokenPayload);
+        try {
+            Mac mac = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(tokenSecret.getBytes(), "HmacSHA256");
+            try {
+                mac.init(secretKeySpec);
+                tokenSignature = encoder.encodeToString(mac.doFinal(tokenSignature.getBytes()));
+            } catch (InvalidKeyException e) {
+                e.printStackTrace();
+                return JWT_FAILURE;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return JWT_FAILURE;
+        }
+
+        // Form the token
+        return tokenHeaders + "." + tokenPayload + "." + tokenSignature;
+    }
+}
--- src/User.java
+++ src/User.java
@@ -3,36 +3,21 @@
  */
 
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
-import java.security.SecureRandom;
-import java.security.NoSuchAlgorithmException;
-import java.security.InvalidKeyException;
-
-import java.util.Base64;
-
-import java.math.BigInteger;
+import java.util.Scanner;
 
 public class User extends Object {
-    public static final String TOKEN_FAILURE = "false";
+    private JWT token;
 
-    public static String CHARSET = "UTF-8";
-
-    private String tokenSecret;
     private int id = 0;
     private String username = "Not Logged In";
     private String password = "";
 
     User() {
-        this(new BigInteger(130, new SecureRandom()).toString(32), "UTF-8");
+        this(JWT.fromEnv());
     }
 
-    User(String mTokenSalt, String charset) {
-        tokenSecret = mTokenSalt;
-        CHARSET = charset;
+    User(JWT mToken) {
+        token = mToken;
     }
 
     private boolean login() {
@@ -40,27 +25,6 @@ public class User extends Object {
     }
 
     public boolean fromStream(InputStream in) {
-//        java.util.Scanner input = new java.util.Scanner(in);
-//        input.useDelimiter(", ");
-//        // Parse in the values
-//
-//        if (input.hasNext()) {
-//            name = input.next();
-//        } else {
-//            return false;
-//        }
-//
-//        if (input.hasNext()) {
-//            age = input.nextInt();
-//        } else {
-//            return false;
-//        }
-//
-//        if (input.hasNext()) {
-//            rate = input.nextFloat();
-//        } else {
-//            return false;
-//        }
         return true;
     }
 
@@ -71,39 +35,17 @@ public class User extends Object {
 
     public String Token() {
         if (!login()) {
-            return TOKEN_FAILURE;
-        }
-        String tokenHeaders = "{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
-        String tokenPayload = String.format("{\"uid\": \"%d\",\"username\": \"%s\"}", id, username);
-
-        // Base64 encode them all
-        Base64.Encoder encoder = Base64.getUrlEncoder();
-        try {
-            tokenHeaders = encoder.encodeToString(tokenHeaders.getBytes(CHARSET));
-            tokenPayload = encoder.encodeToString(tokenPayload.getBytes(CHARSET));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            return TOKEN_FAILURE;
+            return JWT.JWT_FAILURE;
         }
 
-        // Add the secret
-        String tokenSignature = String.format("%s.%s", tokenHeaders, tokenPayload);
-        try {
-            Mac mac = Mac.getInstance("HmacSHA256");
-            SecretKeySpec secretKeySpec = new SecretKeySpec(tokenSecret.getBytes(), "HmacSHA256");
-            try {
-                mac.init(secretKeySpec);
-                tokenSignature = encoder.encodeToString(mac.doFinal(tokenSignature.getBytes()));
-            } catch (InvalidKeyException e) {
-                e.printStackTrace();
-                return TOKEN_FAILURE;
-            }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return TOKEN_FAILURE;
-        }
+        return token.toString(this);
+    }
 
-        // Form the token
-        return tokenHeaders + "." + tokenPayload + "." + tokenSignature;
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
